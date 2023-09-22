f1ee93408de7ae3a16c1b0b166a305864395bed7
Taigo Ito <webmaster@qwel.design>
2023-09-22 16:09:21 +0900

    Set logo behavior and activate the preloader

--- js/init.js
+++ js/init.js
@@ -20,14 +20,40 @@ new EvilIcons();
 import Fader from './_fader.js';
 new Fader();
 
-// スクロールを監視し、ヘッダー色を変更
+// Preloader
+import Preloader from './_preloader.js';
+new Preloader();
+
+// ヘッダーとロゴ色の管理
 const header = document.querySelector('.--transparent');
 if (header) {
+
+  // ロゴを変更
+  const logo = document.querySelector('.gNav__siteBrand img');
+  if (window.innerWidth >= 1200) {
+    logo.setAttribute('src', './assets/logo_miyazakifarm_white.svg');
+  }
+
+  // ヘッダーホバー時、ロゴを変更
+  header.addEventListener('mouseover', () => {
+    logo.setAttribute('src', './assets/logo_miyazakifarm.svg');
+  });
+  header.addEventListener('mouseout', () => {
+    if (window.innerWidth >= 1200 && !(window.innerHeight < window.scrollY)) {
+      logo.setAttribute('src', './assets/logo_miyazakifarm_white.svg');
+    }
+  });
+
+  // スクロールを監視し、ヘッダー色を変更
   window.addEventListener('scroll', () => {
     if (window.innerHeight < window.scrollY) {
       header.classList.remove('--transparent');
+      logo.setAttribute('src', './assets/logo_miyazakifarm.svg');
     } else {
       header.classList.add('--transparent');
+      if (window.innerWidth >= 1200) {
+        logo.setAttribute('src', './assets/logo_miyazakifarm_white.svg');
+      }
     }
   });
 }
